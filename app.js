cd96f5d2fde930e785382855f01aa070fe96509a
조아 <parkjoah@naver.com>
2024-12-23 20:51:43 +0900

    feat: loginSubmit

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-// const loginForm = document.querySelector("#login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
+const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const loginButton = document.querySelector("#login-form button");
+const greeting = document.querySelector("#greeting");
 
-//value > input 안에 있는 텍스트
+const HIDDEN_CLASSNAME = "hidden"; //관습 : string만 포함된 변수는 대문자로 표기함. string 저장하고 싶을 때 사용
 
-function onLoginBtnClick(){
-    console.log(loginInput.value);
+function onLoginSubmit(event){ 
+	event.preventDefault(); 					
+	loginForm.classList.add("hidden");	
+								
+	const username = loginInput.value;
+	greeting.innerText = `Hello ${username}`;
+	greeting.classList.remove("hidden");
+	
+	console.log(username);
+}
 
-};
-loginButton.addEventListener("click",onLoginBtnClick);
+loginForm.addEventListener("submit",onLoginSubmit);
