48e7ab7a45dbf87fc4594a50851532f21e0e8079
조아 <parkjoah@naver.com>
2024-12-24 02:36:41 +0900

    feat : localStorage

--- app.js
+++ app.js
@@ -3,16 +3,26 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"; //관습 : string만 포함된 변수는 대문자로 표기함. string 저장하고 싶을 때 사용
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event){ 
 	event.preventDefault(); 					
-	loginForm.classList.add("hidden");	
-								
+	loginForm.classList.add(HIDDEN_CLASSNAME);	
 	const username = loginInput.value;
-	greeting.innerText = `Hello ${username}`;
-	greeting.classList.remove("hidden");
-	
-	console.log(username);
+	localStorage.setItem(USERNAME_KEY,username);//localStorage저장
+	paintGreetings(username);
 }
 
-loginForm.addEventListener("submit",onLoginSubmit);
+function paintGreetings(username){
+	greeting.innerText = `Hello ${username}`;
+	greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener("submit",onLoginSubmit);
+}else{
+	paintGreetings(savedUsername)
+}
