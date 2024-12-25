4f5b79bed991013fc0f65b950b85ffb271105f0f
조아 <parkjoah@naver.com>
2024-12-25 15:57:03 +0900

    feat: clock

---
+++ clock.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock(){
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+getClock() //최초 출력하고 반복
+setInterval(getClock,1000);
