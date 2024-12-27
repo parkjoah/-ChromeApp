32d9329cf655190c4452fbe8f0cb6070b884e8a2
조아 <parkjoah@naver.com>
2024-12-27 16:17:25 +0900

    feat: toDoList delete

--- todo.js
+++ todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintTodo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const btn = document.createElement("button");
+    btn.innerText="❌";
+    btn.addEventListener("click",deleteToDo);
+    li.appendChild(span);
+    li.appendChild(btn);
     toDoList.appendChild(li);
 }
 
