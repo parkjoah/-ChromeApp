85c895defd7e9d2898f834339a9f4001e45fa4f9
조아 <parkjoah@naver.com>
2024-12-27 23:02:20 +0900

    feat: todos save,load

--- todo.js
+++ todo.js
@@ -2,6 +2,15 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY ="todos";
+
+//array 만들어주기
+let toDos =[];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     const li = event.target.parentElement;
     li.remove();
@@ -23,8 +32,19 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; //newTodo : string 값
     toDoInput.value="";
+    toDos.push(newTodo); //array에 넣기
     paintTodo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+console.log(savedToDos);
+if(savedToDos !==null ){
+    const parsedToDo = JSON.parse(savedToDos);
+    toDos = parsedToDo;
+    parsedToDo.forEach(paintTodo);
+}
 
