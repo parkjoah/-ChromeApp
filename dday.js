f738203853ec9f13ad2164da88fd645e18291c3c
조아 <parkjoah@naver.com>
2025-02-03 18:29:10 +0900

    feat: dDay dayCount & spanpop

--- dday.js
+++ dday.js
@@ -6,23 +6,28 @@ const dDayPop = document.getElementById("dDayPop");
 const dDayDate = dDayForm.querySelector("#dDayDate");
 const dDayDelBtn = dDayForm.querySelector("#dDaydelBtn");
 
-const span = dDayBlock.querySelector("span");
-function paintdDay(newdDay) {
+const dDaySpan = dDayBlock.querySelector("#dDaySpan");
+const dDateSpan = dDayBlock.querySelector("#dDateSpan");
+function paintdDay(newdDay, targetDate) {
   console.log(newdDay);
-  span.innerText = newdDay;
-  spanShow();
+  dDaySpan.innerText = newdDay;
+  dDateSpan.innerText = "D - " + countDday(targetDate);
+  dDaySpanShow();
+  popDday();
 }
 
 function dDaySubmit(event) {
   event.preventDefault();
   // console.log(dDayInput.value);
   const newdDay = dDayInput.value;
+  const newdDate = dDayDate.value;
+  const targetDate = new Date(newdDate); //형식 맞추기...
   // console.log(dDayDate.value);
   dDayDate.value == ""
     ? alert("날짜를 설정해주세요")
     : dDayInput.value == ""
     ? alert("디데이를 입력해주세요")
-    : paintdDay(newdDay);
+    : paintdDay(newdDay, targetDate);
   dDayInput.value = "";
   dDayDate.value = "";
 }
@@ -34,20 +39,27 @@ function popDday() {
 }
 
 function dDayDel() {
-  span.innerText = "";
-  // console.log(span.innerText);
-  spanShow();
+  dDaySpan.innerText = "";
+  // console.log(dDaySpan.innerText);
+  dDaySpanShow();
 }
 
 dDayForm.addEventListener("submit", dDaySubmit);
 
 dDayPopBtn.addEventListener("click", popDday);
 dDayDelBtn.addEventListener("click", dDayDel);
+dDayBlock.addEventListener("click", popDday);
 
-function spanShow() {
-  span.innerText !== ""
+function dDaySpanShow() {
+  dDaySpan.innerText !== ""
     ? dDayBlock.classList.remove(HIDDEN_CLASSNAME)
     : dDayBlock.classList.add(HIDDEN_CLASSNAME);
+}
+
+function countDday(newdDate) {
+  const todayDate = new Date();
+  const dayCount = Math.ceil((newdDate - todayDate) / (1000 * 60 * 60 * 24));
+  return dayCount;
 }
 
 // 헷갈리니까 해야 되는 거 메모//
@@ -55,9 +67,10 @@ function spanShow() {
 // 누적해서 써지는데 이전 거 사라지도록 -ok
 // localStorage에 저장 삭제 기능 (유지)
 // 날짜 설정 -ok
-// 날짜 기반 남은 날 계산해서 표시
+// 날짜 기반 남은 날 계산해서 표시 -ok
 // 당일이 되면 메세지가 뜨도록 함
 // 삭제 / 저장 버튼 만들고 -ok
 // submit 되면 입력 값 지우기 -ok
-// 디데이 위에 생성된 span 눌러도 창이 뜨게 함
+// 디데이 위에 생성된 span 눌러도 창이 뜨게 함 -ok
 // 디데이 창 디자인 바꾸기
+// 0시에 디데이 일 수 바뀌게?
