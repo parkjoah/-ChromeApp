a96a30dc3baa55cba0614aa9542b8905e72f430a
조아 <parkjoah@naver.com>
2025-02-14 21:29:00 +0900

    feat : bgImg btn

--- background.js
+++ background.js
@@ -1,11 +1,21 @@
 const images = ["CA01.jpg", "CA02.jpg", "CA03.jpg", "CA04.jpg", "CA05.jpg"];
 
-const chosenImage = images[Math.floor(Math.random() * images.length)];
+const imgChangeBtn = document.querySelector("#container button:nth-child(2)");
 
 const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-
-// console.log(bgImage);
+bgImage.src = `img/CA05.jpg`;
 
 document.body.appendChild(bgImage);
+
+let bgNow = "";
+
+function imgChange() {
+  let chosenImage;
+  do {
+    chosenImage = images[Math.floor(Math.random() * images.length)];
+  } while (bgNow == chosenImage);
+  bgNow = chosenImage;
+  bgImage.src = `img/${chosenImage}`;
+}
+
+imgChangeBtn.addEventListener("click", imgChange);
